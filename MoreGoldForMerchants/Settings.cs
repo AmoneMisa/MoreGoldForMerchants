481ba8dea09f6fc8c6d515eb7f9d46216d14b814
WhitesLove <kubai.rita2@yandex.ru>
2022-07-12 18:15:04 +0300

    Initial commit

---
+++ MoreGoldForMerchants/Settings.cs
@@ -0,0 +1,10 @@
+﻿using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace MoreGoldForMerchants
+{
+    public class Settings
+    {
+        [SynthesisTooltip("Amount gold.")]
+        public int AmountGold = 15000;
+    }
+}
